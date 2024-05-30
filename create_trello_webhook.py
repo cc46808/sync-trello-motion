59bfc08fe2d177f24cc0606b38cff2219a78bde8
cc46808 <cc46808@gmail.com>
2024-05-30 17:39:39 -0400

    improved error handling and debug output to ensure everything is correctly set

--- create_trello_webhook.py
+++ create_trello_webhook.py
@@ -7,24 +7,28 @@ TRELLO_BOARD_ID = os.getenv('TRELLO_BOARD_ID')
 CALLBACK_URL = os.getenv('CALLBACK_URL')
 
 def create_trello_webhook(callback_url, id_model, description="Trello Webhook"):
-    url = f"https://api.trello.com/1/tokens/{TRELLO_API_TOKEN}/webhooks/"
+    url = "https://api.trello.com/1/webhooks/"
     query = {
         'key': TRELLO_API_KEY,
-        'token': TRELLO_API_TOKEN,
+        'token': TRELLO_API_TOKEN
+    }
+    data = {
         'callbackURL': callback_url,
         'idModel': id_model,
         'description': description
     }
-    
-    print(f"URL: {url}")
-    print(f"Parameters: {query}")
 
-    response = requests.post(url, params=query)
+    print(f"URL: {url}")
+    print(f"Query Params: {query}")
+    print(f"Data: {data}")
+
+    response = requests.post(url, params=query, json=data)
     try:
         response.raise_for_status()
     except requests.exceptions.HTTPError as e:
         print(f"HTTPError: {e}")
         print(f"Response Text: {response.text}")
+        return None
     return response.json()
 
 webhook_response = create_trello_webhook(CALLBACK_URL, TRELLO_BOARD_ID)
