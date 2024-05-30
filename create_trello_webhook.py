a9d6cf78388907f7dd1303dbec12b34ff929d101
cc46808 <cc46808@gmail.com>
2024-05-30 17:45:41 -0400

    adapt it to your specific needs by incorporating your environment variables

--- create_trello_webhook.py
+++ create_trello_webhook.py
@@ -1,35 +1,42 @@
 import os
 import requests
+import json
 
+# Fetching environment variables
 TRELLO_API_KEY = os.getenv('TRELLO_API_KEY')
 TRELLO_API_TOKEN = os.getenv('TRELLO_API_TOKEN')
 TRELLO_BOARD_ID = os.getenv('TRELLO_BOARD_ID')
 CALLBACK_URL = os.getenv('CALLBACK_URL')
 
-def create_trello_webhook(callback_url, id_model, description="Trello Webhook"):
+def create_trello_webhook(callback_url, id_model, api_key, api_token):
     url = "https://api.trello.com/1/webhooks/"
-    query = {
-        'key': TRELLO_API_KEY,
-        'token': TRELLO_API_TOKEN
+
+    headers = {
+        "Accept": "application/json"
     }
-    data = {
+
+    query = {
         'callbackURL': callback_url,
         'idModel': id_model,
-        'description': description
+        'key': api_key,
+        'token': api_token
     }
 
-    print(f"URL: {url}")
-    print(f"Query Params: {query}")
-    print(f"Data: {data}")
+    response = requests.request(
+        "POST",
+        url,
+        headers=headers,
+        params=query
+    )
 
-    response = requests.post(url, params=query, json=data)
     try:
         response.raise_for_status()
     except requests.exceptions.HTTPError as e:
         print(f"HTTPError: {e}")
         print(f"Response Text: {response.text}")
         return None
-    return response.json()
+    
+    return json.loads(response.text)
 
-webhook_response = create_trello_webhook(CALLBACK_URL, TRELLO_BOARD_ID)
-print(webhook_response)
+webhook_response = create_trello_webhook(CALLBACK_URL, TRELLO_BOARD_ID, TRELLO_API_KEY, TRELLO_API_TOKEN)
+print(json.dumps(webhook_response, sort_keys=True, indent=4, separators=(",", ": ")))
